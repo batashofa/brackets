50e4758f72d8a5c9c4c23e682855c895ff880d40
batashofa <batashofa@gmail.com>
2021-08-10 12:52:38 +0300

    feat: added brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let length = str.length;
+    let res;
+    if (str.length % 2 !== 0) {
+        res = false;
+    }
+    let arr = str.split('');
+    let bracketsConfig1 = bracketsConfig.flat();
+    for (let i = 0; i < bracketsConfig1.length; i++) {
+        let array = arr.filter(n => bracketsConfig1.indexOf(n) === -1);
+        if (array.length > 0) {
+            res = false;
+        }
+    }
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            str = str.replace(bracketsConfig[j].join(''), '');
+        }
+    }
+    str.length > 0 ? res = false : res = true;
+    return res;
 }
+
+
+
+
